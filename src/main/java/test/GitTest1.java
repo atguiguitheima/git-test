f07827020089136807ee400cd5fc96eea8207882
itheima <itheima@itcast.com>
2024-03-22 12:49:58 +0800

    hot-fix four commit

--- src/main/java/test/GitTest1.java
+++ src/main/java/test/GitTest1.java
@@ -5,9 +5,9 @@ public class GitTest1 {
         System.out.println("git");
         System.out.println("git");
         System.out.println("git");
-        System.out.println("hot fix");
-        System.out.println("hot fix");
-        System.out.println("hot fix");
-        System.out.println("hot fix");
+        System.out.println("hot fix4");
+        System.out.println("hot fix4");
+        System.out.println("hot fix4");
+        System.out.println("hot fix4");
     }
 }
