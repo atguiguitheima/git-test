86bf0c3fe782a98abb68461764da6d2a05d56242
itheima <itheima@itcast.com>
2024-03-22 10:09:22 +0800

    first commit

---
+++ src/main/java/test/GitTest1.java
@@ -0,0 +1,9 @@
+package test;
+
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
